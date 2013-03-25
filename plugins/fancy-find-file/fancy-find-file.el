11ca966cc0fadf369c50a63921a27a2d7c342d5f
Chris Kowalik <chris@nu7hat.ch>
2013-03-25 03:23:04 -0300

    added fancy file loader plugin

---
+++ plugins/fancy-find-file/fancy-find-file.el
@@ -0,0 +1,25 @@
+;;; fancy-find-file.el --- find file with extra features.
+
+;;; Commentary:
+
+;; Plugins adds ability to open file with a line selected.
+
+;;; Code:
+
+(defadvice find-file (around find-file-line-number
+                             (filename &optional wildcards)
+                             activate)
+  "Turn files like file.cpp:14 into file.cpp and going to the 14-th line."
+  (save-match-data
+    (let* ((matched (string-match "^\\(.*\\):\\([0-9]+\\):?$" filename))
+           (line-number (and matched
+                             (match-string 2 filename)
+                             (string-to-number (match-string 2 filename))))
+           (filename (if matched (match-string 1 filename) filename)))
+      ad-do-it
+      (when line-number
+        ;; goto-line is for interactive use
+        (goto-char (point-min))
+        (forward-line (1- line-number))))))
+
+(provide 'fancy-find-file)
