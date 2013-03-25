f5f814b85250b3ae11a810f1362c6ca0211e1e90
Chris Kowalik <chris@nu7hat.ch>
2013-03-25 03:25:57 -0300

    fixes in dired-omit-mode

--- hooks/dired-omit-mode.el
+++ hooks/dired-omit-mode.el
@@ -1,2 +1,6 @@
-;; Enable omit mode when entering into dired-mode
+;;; dired-omit-mode.el ---  Enable omit mode when entering
+;;; into dired-mode
+
+;;; Code:
+
 (add-hook 'dired-mode-hook (lambda() (dired-omit-mode 1)))
