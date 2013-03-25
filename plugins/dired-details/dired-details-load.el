eb2002c5b2572d93e35897d6963a4420616d74a2
Chris Kowalik <chris@nu7hat.ch>
2013-03-25 03:22:17 -0300

    added dired details hiding

---
+++ plugins/dired-details/dired-details-load.el
@@ -0,0 +1,12 @@
+;;; dired-details-load.el --- Loads dired-details plugin.
+
+;;; Commentary:
+
+;; See dired-details.el and dired-details+.el documentation.
+
+;;; Code:
+
+(require 'dired-details)
+(require 'dired-details+)
+
+(provide 'dired-details-load)
