82447c6bf94251adaf57cf28d5a698ed9ffb3f71
Chris Kowalik <chris@nu7hat.ch>
2013-03-25 04:40:10 -0300

    added buffer managing plugins (ibuffer and buffer ring)

---
+++ plugins/buffer-ring/buffer-ring-load.el
@@ -0,0 +1,11 @@
+;;; buffer-ring-load.el --- Loads buffer-ring plugin.
+
+;;; Commentary:
+
+;; See buffer-ring.el documentation.
+
+;;; Code:
+
+(require 'buffer-ring)
+
+(provide 'buffer-ring-load)
