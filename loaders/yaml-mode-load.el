1caafb327e061666d77fff6c695ef8094a2de075
Chris Kowalik <chris@nu7hat.ch>
2013-04-08 06:34:29 -0300

    configured yaml mode

--- loaders/yaml-mode-load.el
+++ loaders/yaml-mode-load.el
@@ -5,10 +5,11 @@
 ;; See yaml-mode.el for documentation.
 
 (autoload 'yaml-mode "yaml-mode" "\
-Major mode for editing yaml documents text.
+Major mode for editing yaml documents.
 
 \(fn)" t nil)
 
-(add-to-list 'auto-mode-alist (cons "\\.yaml$" #'yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
 
 (provide 'yaml-mode-load)
